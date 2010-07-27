d66e0da03e28f411362509e7fa3fbdfb61ed92de
andrey.sobko@gmail.com <andrey.sobko@gmail.com@a5ce5800-1bf5-11df-85e6-9b4bd7dd5138>
2010-07-27 09:40:34 +0000

    common device info transfer struct

--- trunk/inc/devctrlex.h
+++ trunk/inc/devctrlex.h
@@ -6,4 +6,18 @@ DEFINE_GUID( GET_MEDIA_SERIAL_NUMBER_GUID,
     0xe636cd65,
     0x4021, 0x4d20, 0x97, 0xec, 0x5a, 0xe2, 0x31, 0x89, 0x56, 0x6b );
 
+#define _DEVCTRL_DEVICEINFO_VER1    0x1
+
+typedef struct _DEVCTRL_DEVICEINFO
+{
+    ULONG       Version;
+    ULONG       BusId;                          /// \todo
+    ULONG       IsRemovable;                    /// \todo
+    ULONG       IsIdUnique;
+    ULONG       IdOffset;
+    ULONG       IdLenght;
+    ULONG       ClassIdOffset;
+    ULONG       ClassIdLength;
+    UCHAR       Data[1];
+} DEVCTRL_DEVICEINFO, *PDEVCTRL_DEVICEINFO;
 #endif // __devctrlex_h
